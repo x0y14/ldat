ab3b0f3fbad8a8a528fd13848dcf57ff01b7522e
x0y14 <ya01ma08@gmail.com>
2022-05-20 14:25:19 +0900

    string

--- common_datatype.go
+++ common_datatype.go
@@ -18,3 +18,21 @@ const (
 	Ui64
 	Ui32
 )
+
+// \s?([A-z0-9]+)\s?\n?
+
+var dtypes = [...]string{
+	Void:   "Void",
+	String: "String",
+	Char:   "Char",
+	Float:  "Float",
+	Double: "Double",
+	I64:    "I64",
+	I32:    "I32",
+	Ui64:   "Ui64",
+	Ui32:   "Ui32",
+}
+
+func (d Datatype) String() string {
+	return dtypes[d]
+}
